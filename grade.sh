3c21888e4e49307ff73f3d9a1fbf76e248409c32
anna <125645248+annadoannn@users.noreply.github.com>
2023-11-28 08:48:13 -0800

    Updated

--- grade.sh
+++ grade.sh
@@ -12,5 +12,22 @@ echo 'Finished cloning'
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    #echo "you exist"
+    cp TestListExamples.java ./student-submission
+    javac -cp $CPATH *.java
+    cp -r student-submission/*.java grading-area
+    if [ $? -eq 0 ]
+    then
+        echo "All tests passed"
+        echo Your grade is 100%
+        exit 0
+    fi
+else 
+    echo "wrong file(s) submitted"
+    exit
+fi
+
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
